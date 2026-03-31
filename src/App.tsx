8d487ada06ff49608bd4b816d0a927231f1b2c36
Natsuki Iida <natsuki.iida0511@gmail.com>
2026-03-31 11:14:57 +0200

    minor fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,12 @@
-// import { lazy } from "react";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import { Paths } from "./types/enum"
+import { HashRouter } from "react-router-dom";
 
-// const Home = lazy(() => import("./components/Home/Home"));
-// const Profile = lazy(() => import("./components/Profile/Profile"));
-// const Works = lazy(() => import("./components/Works/Works"));
-
-// function App() {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path={Paths.HOME} element={<Home />}/>
-//         <Route path={Paths.PROFILE} element={<Profile />}/>
-//         <Route path={Paths.WORKS} element={<Works />} />
-//       </Routes>
-//     </Router>
-//   )
-// }
-
-// export default App;
-
-import { BrowserRouter as Router } from "react-router-dom";
 import AnimatedRoutes from "./components/AnimatedRoutes";
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <AnimatedRoutes />
-    </Router>
+    </HashRouter>
   )
 }
 
